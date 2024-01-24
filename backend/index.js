e16e4b7f78ec379090bc2a1dfcf9a1d1a35cd8a0
furkantaglik <furkantaglik2334@gmail.com>
2024-01-25 00:06:19 +0300

    özel odalar oluşturuldu

--- backend/index.js
+++ backend/index.js
@@ -17,14 +17,27 @@ import userRouter from "./api/userRouter.js";
 dotenv.config();
 app.use(cors({}));
 app.use(express.json());
+
 io.on("connection", (socket) => {
+  socket.on("join room", (roomID) => {
+    socket.join(roomID);
+    console.log(`${socket.id} joined room ${roomID}`);
+  });
+
+  socket.on("leave room", (roomID) => {
+    socket.leave(roomID);
+    console.log(`${socket.id} left room ${roomID}`);
+  });
+
+  // Özel mesajları dinleme
   socket.on("private message", (data) => {
     const { content, to, from, image } = data;
+    const roomID = `${from}-${to}`;
+
     io.emit("private message", content, to, from, image);
-    console.log("mesaj", content, "kime", to, "from", from);
+    console.log(`Private message sent to room ${roomID}: ${content}`);
   });
 });
-
 app.use("/api/user", userRouter);
 server.listen(5000, () => {
   console.log("Uygulama 5000 portunda çalışıyor.");
